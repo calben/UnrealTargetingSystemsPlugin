460783cf32c2431afa990a7c67a1a34831497baf
Calem Bendell <calem.j.bendell@gmail.com>
2017-04-05 12:18:46 -0400

    documentation for targeting system component

--- Source/TargetingSystemsPlugin/Public/Components/TargetingSystemComponent.h
+++ Source/TargetingSystemsPlugin/Public/Components/TargetingSystemComponent.h
@@ -33,7 +33,7 @@ class TARGETINGSYSTEMSPLUGIN_API UTargetingSystemComponent : public USceneCompon
 
 	/// @cond
 	GENERATED_BODY()
-	/// @endcond
+		/// @endcond
 
 public:
 	// Sets default values for this component's properties
@@ -46,24 +46,56 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		/// <summary>
+		/// If true, the component will retarget on TickComponent.
+		/// </summary>
 		bool bIsTargeting = true;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		/// <summary>
+		/// If not null, the targeted actor.
+		/// </summary>
+		/// <warning>
+		/// Make sure to perform a null check if accessing this value.
+		/// </warning>
 		class AActor* CurrentTarget;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		/// <summary>
+		/// If not null, the actor target before the current actor.
+		/// </summary>
+		/// It is often useful to know the previously targeted actor,
+		/// particularly if some effect is being applied to the targeted
+		/// actor and you don't want to edit more functionality into 
+		/// @OnTargetChanged()
 		class AActor* PreviousTarget;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		/// <summary>
+		/// If true, draws debug lines for the targeting trace.
+		/// </summary>
 		bool bDrawDebug = false;
 
 	UFUNCTION(BlueprintCallable, Category = Targeting)
+		/// <summary>
+		/// The function used to calculate a new target.
+		/// </summary>
+		/// This function is typically overridden in children
+		/// of this component.
 		virtual void SetCurrentTarget();
 
 	UFUNCTION(BlueprintCallable, Category = Targeting)
+		/// <summary>
+		/// Used to trigger behaviour when the current target changes.
+		/// </summary>
+		/// TODO: make this implementable
 		void OnTargetChanged();
 
 	UFUNCTION(BlueprintCallable, Category = Rendering)
+		/// <summary>
+		/// Sets the custom render depth to false on previous target 
+		/// and true on current to enable highlighting effects.
+		/// </summary>
 		void UpdateHighlighting();
 
 };
